ee35e88ee821a3c44a5f72c82818ad71f3e5395d
Audyari Wiyono <audy123ari@gmail.com>
2024-07-07 21:23:49 +0700

    Membuat Project Maven

---
+++ src/main/java/programmer/butocakil/todolist/App.java
@@ -0,0 +1,13 @@
+package programmer.butocakil.todolist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

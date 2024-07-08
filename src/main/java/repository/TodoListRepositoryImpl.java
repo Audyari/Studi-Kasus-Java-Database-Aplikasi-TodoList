39fa0f7b41b9a42d6d646a8b9a246d1c61eb5c38
Audyari Wiyono <audy123ari@gmail.com>
2024-07-08 09:35:54 +0700

    Repository : Menghapus Todolist

--- src/main/java/repository/TodoListRepositoryImpl.java
+++ src/main/java/repository/TodoListRepositoryImpl.java
@@ -5,9 +5,10 @@ import entity.Todolist;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class TodoListRepositoryImpl  implements TodoListRepository {
+public class TodoListRepositoryImpl implements TodoListRepository {
 
     public Todolist[] data = new Todolist[10];
 
@@ -64,19 +65,40 @@ public class TodoListRepositoryImpl  implements TodoListRepository {
     @Override
     public boolean remove(Integer number) {
 
-        if ((number - 1) >= data.length) {
-            return false;
-        } else if (data[number - 1] == null) {
-            return false;
+        if (isExists(number)) {
+            String sql = "DELETE FROM todolist WHERE id = ?";
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement(sql)) {
+                statement.setInt(1, number);
+                statement.executeUpdate();
+
+                return true;
+            } catch (SQLException exception) {
+                throw new RuntimeException(exception);
+            }
         } else {
-            for (int i = (number - 1); i < data.length; i++) {
-                if (i == (data.length - 1)) {
-                    data[i] = null;
+            return false;
+        }
+
+
+    }
+
+    private boolean isExists(Integer number) {
+        String sql = "SELECT id FROM todolist WHERE id = ?";
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setInt(1, number);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    return true;
                 } else {
-                    data[i] = data[i + 1];
+                    return false;
                 }
             }
-            return true;
+
+        } catch (SQLException exception) {
+            throw new RuntimeException(exception);
         }
     }
 }
